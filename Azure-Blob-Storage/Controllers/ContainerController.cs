7e31f79a2e93c066a02ee71a1933127173ff85a9
DalimAli <dalim3281@gmail.com>
2025-02-25 07:01:39 +0600

    Container controller with creation, deletion and listof info created

---
+++ Azure-Blob-Storage/Controllers/ContainerController.cs
@@ -0,0 +1,41 @@
+﻿using Azure_Blob_Storage.Models;
+using Azure_Blob_Storage.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Azure_Blob_Storage.Controllers;
+
+public class ContainerController(IContainerService containerService) : Controller
+{
+
+    public async Task<IActionResult> Index()
+    {
+        var containerNames = await containerService.GetAllContainer();
+
+        
+        return View(containerNames);
+    }
+
+    public async Task<IActionResult> Create()
+    {
+        return View(new Container());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateContainer(Container container)
+    {
+        await containerService.CreateContainer(container.Name);
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> Delete(string containerName)
+    {
+        await containerService.DeleteContainer(containerName);
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> GetAllContainerAndBlobs()
+    {
+        return View(await containerService.GetAllContainerAndBlobs());
+    }
+
+}
---
+++ Azure-Blob-Storage/Models/Container.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Azure_Blob_Storage.Models;
+
+public class Container
+{
+    [Required]
+    public string Name { get; set; }
+}
